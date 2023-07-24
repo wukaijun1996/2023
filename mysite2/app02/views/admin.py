8fec85225f35dcd8a06c54a765a3dfc1a9b62da2
wukaijun1996 <1172917145@qq.com>
2023-07-25 00:55:24 +0800

    admin reset

--- mysite2/app02/views/admin.py
+++ mysite2/app02/views/admin.py
@@ -57,6 +57,42 @@ class AdminModelForm(BootStrapModelForm):
             raise ValidationError("两次输入不一致")
 
 
+class AdminEditModelForm(BootStrapModelForm):
+    class Meta:
+        model = Admin
+        fields = ["username"]
+
+
+class AdminResetModelForm(BootStrapModelForm):
+    confirm_password = forms.CharField(
+        label="确认密码",
+        # render_value=True 两次输入不一致内容不清空
+        widget=forms.PasswordInput(render_value=True)
+    )
+
+    class Meta:
+        model = Admin
+        fields = ["password"]
+        widgets = {
+            "password": forms.PasswordInput(render_value=True)
+        }
+
+    def clean_password(self):
+        pwd = self.cleaned_data.get("password")
+        md5_pwd = md5(pwd)
+
+        # 去校验
+
+        return md5(self.cleaned_data.get("password"))
+
+    def clean_confirm_password(self):
+        if self.cleaned_data.get("password") == md5(self.cleaned_data.get("confirm_password")):
+            # 返回什么 此字段以后保存到数据库就是什么
+            return md5(self.cleaned_data.get("confirm_password"))
+        else:
+            raise ValidationError("两次输入不一致")
+
+
 def admin_add(request):
     """添加管理员"""
     title = "新建管理员"
@@ -74,12 +110,6 @@ def admin_add(request):
     return render(request, "change.html", {"title": title, "form": form})
 
 
-class AdminEditModelForm(BootStrapModelForm):
-    class Meta:
-        model = Admin
-        fields = ["username"]
-
-
 def admin_edit(request, nid):
     """编辑管理员"""
     row_object = Admin.objects.filter(id=nid).first()
@@ -94,3 +124,26 @@ def admin_edit(request, nid):
         form.save()
         return redirect("/admin/list")
     return render(request, "change.html", {"title": title, "form": form})
+
+
+def admin_delete(request, nid):
+    """删除管理员"""
+
+    Admin.objects.filter(id=nid).delete()
+    return redirect("/admin/list")
+
+
+def admin_reset(request, nid):
+    """重置密码"""
+    row_object = Admin.objects.filter(id=nid).first()
+    if not row_object:
+        return redirect("/admin/list")
+    title = "重置密码 - {}".format(row_object.username)
+    if request.method == "GET":
+        form = AdminResetModelForm()
+        return render(request, "change.html", {"form": form, "title": title})
+    form = AdminResetModelForm(data=request.POST, instance=row_object)
+    if form.is_valid():
+        form.save()
+        return redirect("/admin/list")
+    return render(request, "change.html", {"form": form, "title": title})
