226b7d452ebefd241ab1102dc9f702baf3dc9cf8
wukaijun1996 <1172917145@qq.com>
2023-07-27 21:56:28 +0800

    auth complete

--- mysite2/app02/views/account.py
+++ mysite2/app02/views/account.py
@@ -8,6 +8,7 @@ from app02.utils.encrypt import md5
 from app02.utils.code import check_code
 from io import BytesIO
 
+
 class LoginForm(BootStrapForm):
     username = forms.CharField(
         label="用户名",
@@ -16,6 +17,12 @@ class LoginForm(BootStrapForm):
     password = forms.CharField(
         label="密码",
         widget=forms.PasswordInput(attrs={"class": "form-control"}, render_value=True)
+    )
+
+    code = forms.CharField(
+        label="验证码",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        required=True
     )
 
     def clean_password(self):
@@ -32,6 +39,14 @@ def login(request):
     if form.is_valid():
         # forms.Form 只能返回输入后的数据form.cleaned_data
         print(form.cleaned_data)
+        # 拿到输入的验证码的值
+        user_input_code = form.cleaned_data.pop("code")
+        # 后端生成的真正的图片的值
+        code = request.session.get("image_code", "")
+        if code.upper() != user_input_code.upper():
+            form.add_error("code", "验证码错误")
+            return render(request, "login.html", {"form": form})
+
         # admin_object = Admin.objects.filter(username=form.cleaned_data.get("username"),
         #                                     password=md5(form.cleaned_data.get("password"))).first()
         admin_object = Admin.objects.filter(**form.cleaned_data).first()
@@ -43,6 +58,8 @@ def login(request):
         # 用户名和密码正确
         # 网站生成随机字符串，写到用户浏览器的cookie中，再写入到session中
         request.session["info"] = {"id": admin_object.id, "name": admin_object.username}
+        # session保存七天免登录
+        request.session.set_expiry(60 * 60 * 24 * 7)
         return redirect("/admin/list")
 
     return render(request, "login.html", {"form": form})
@@ -56,8 +73,9 @@ def image_code(request):
     request.session["image_code"] = code_string
     # 给session设置60s超时
     request.session.set_expiry(60)
-    print(code_string)
+    # print(code_string)
 
+    # 将生成的图片保存在缓存中
     stream = BytesIO()
     img.save(stream, "png")
 
